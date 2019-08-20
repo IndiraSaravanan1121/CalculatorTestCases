e193233c4fc9acd8c4321447f361bb7e62ea7645
IndiraSaravananS <indira.saravanan@atmecs.com>
2019-08-20 20:25:57 +0530

    initial stage

---
+++ src/com/testng/calculatorOperation.java
@@ -0,0 +1,25 @@
+package com.testng;
+
+public class calculatorOperation {
+	public double add(int a, int b)
+	{
+		return a+b;
+		
+	}
+	public double sub(int a, int b)
+	{
+		return a-b;
+		
+	}
+	public double mul(int a, int b)
+	{
+		return a*b;
+		
+	}
+	public double div(int a, int b)
+	{
+		return a/b;
+		
+	}
+
+}
